e17443d6cee1df9ad754e3c42eafaa11b9548caf
esauOp <monkeycoss@gmail.com>
2017-09-27 12:13:32 -0500

    Install script (Apps, Links, etc).

--- install.sh
+++ install.sh
@@ -1,4 +1,98 @@
+#!/bin/bash
 
-# Useful Packages
-# brew install htop
+# Installs Homebrew and some of the common dependencies needed/desired for software development
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap homebrew/versions
+brew tap homebrew/dupes
+brew tap Goles/battery
+
+brew update
+brew upgrade --all
+
+# Install packages
+
+apps=(
+  bash-completion2
+  bats
+  battery
+  coreutils
+  diff-so-fancy
+  dockutil
+  ffmpeg
+  fasd
+  gifsicle
+  git
+  git-extras
+  gnu-sed --with-default-names
+  grep --with-default-names
+  hub
+  httpie
+  imagemagick --with-webp
+  jq
+  lynx
+  mackup
+  nano
+  node
+  pandoc
+  peco
+  psgrep
+  python
+  rvm
+  shellcheck
+  ssh-copy-id
+  the_silver_searcher
+  tree
+  unar
+  wget
+  wifi-password
+)
+
+brew install "${apps[@]}"
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+# Install GUI applications
+#brew tap caskroom/cask
+#brew install brew-cask
+#brew tap caskroom/versions
+
+#brew cask install atom
+#brew cask install vlc
+
+#brew cask cleanup
+
+# Bunch of symlinks
+ln -sfv ~/dotfiles/zsh/zshrc.symlink ~/.zsh
+ln -sfv ~/dotfiles/vim/vimrc.symlink ~/.vimrc
+ln -sfv ~/dotfiles/system/alias ~/.alias
+ln -sfv ~/dotfiles/git/gitconfig ~/.gitconfig
+
+
+# Globally install with npm
+
+packages=(
+  get-port-cli
+  gtop
+  historie
+  nodemon
+  npm
+  release-it
+  spot
+  superstatic
+  svgo
+  tldr
+  underscore-cli
+)
+
+npm install -g "${packages[@]}"
